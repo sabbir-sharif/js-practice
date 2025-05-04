c58a1aaffc5aa2eacf16e311b45a257604475dcb
SABBIR SHARIF <149605755+sabbir-sharif@users.noreply.github.com>
2025-05-04 10:01:41 +0000

    data types

---
+++ 01_Basics/02_data_types.js
@@ -0,0 +1,9 @@
+number = 20 // Number
+myName = "Sabbir" // String
+isAlive = true //Boolean
+balance = null // Null. it is not 0
+
+//Object
+//BigInt
+
+console.table([number, myName, isAlive, balance]) 
