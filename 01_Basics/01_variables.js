5b31262b00e4e5630e6d544b190885587ad4ae03
SABBIR SHARIF <149605755+sabbir-sharif@users.noreply.github.com>
2025-05-04 09:58:05 +0000

    const, let, var

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1832 //Cannot change the value. static final in Java
+let email = "sabbir@gmailcom" //variable. 
+var name = "Sabbir Sharif" //ususally no use. it does not support scope
+
+address = "savar, Dhaka" //also variable.
+
+console.log(email)
