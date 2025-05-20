2f86881829b99cd6e8d8b88093c423331b5d4b69
SABBIR SHARIF <149605755+sabbir-sharif@users.noreply.github.com>
2025-05-20 18:38:15 +0000

    String and string's methods

---
+++ 01_Basics/04_string.js
@@ -0,0 +1,37 @@
+//let str = `Bangladesh is my "country"`; Right
+
+//let str = "Bangladesh is my 'country'"; right
+
+//let str = 'Bangladesh is my "country"';
+
+//let str = "Bangladesh is my "country""; wrong
+
+// let str = `Hello 
+//            world
+//            multiple 
+//            line 
+//            string`;
+
+// let str1 = new String("Hello");
+// let str2 = new String("Hello");
+//str1 == str2 false
+//str1 === str2 false
+
+//-------String Methods ------------
+// slice(start, end) [include, exclude) (1, 4) slice from 1 to 3/ (-ve) index means from last
+// substring(start, end) [start_index, end_index) -ve start_index auto make starting index 0.
+// substr(start, length) [index, length] -ve index start means from last it will slice.
+
+//------ String Search Methods----------
+// String indexOf()
+// String lastIndexOf()
+// String search()
+// String match()
+// String matchAll()
+// String includes()
+// String startsWith()
+// String endsWith()
+
+let str = "Hello World";
+
+console.log(str.substring(-2, 4)); 
